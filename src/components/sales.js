6704f7d44289a02e4d4f6642f2b45c12e04e611e
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-06-15 17:51:06 -0700

    Fix details button and add error page

--- src/components/sales.js
+++ src/components/sales.js
@@ -4,6 +4,7 @@ import Header from './header';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import DealBoard from './deal-board';
 import DetailInformation from './detail-information';
+import Error from './error';
 
 export default function Sales() {
   return (
@@ -13,7 +14,8 @@ export default function Sales() {
           <main>
             <Switch>
               <Route exact path="/" component={DealBoard} /> 
-              <Route exact path="/detail-information/:detail-information" component={DetailInformation} />
+              <Route path="/detail-information" component={DetailInformation} />
+              <Route component={Error} />
             </Switch>
           </main>
       </div>
--- src/components/view-details.js
+++ src/components/view-details.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
-// import {Route} from 'react-router-dom';
+import {Link} from "react-router-dom";
 
 import './view-details.css';
 
 export default class ViewDetails extends React.Component {
-    detailsPage() {
-        this.context.history.push("/detail-information/");
-    }
     render() {
         return (
             <div>
-                <button onClick={this.detailsPage} className="btn-details">{this.props.details}</button>
+                <Link to={"/detail-information"}><button class="btn">{this.props.details}</button></Link>
             </div>
-// export default class ViewDetails extends React.Component {            
-    // const Button = () => (
-    //     <Route render={({history}) => (
-    //         <button
-    //             type="button"
-    //             onClick={() => {history.push("/detail-information")}}
-    //         >
-    //         {this.props.details}
-    //         </button>
-    //     )} />
-    // )
-
         )
     }
 }
