46c93034f03881d7e04df92d41256fd4e9fff7ba
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-06-19 20:59:39 -0700

    Update forms using Redux form

--- src/components/pages/edit-deal.js
+++ src/components/pages/edit-deal.js
@@ -3,12 +3,13 @@ import {reduxForm, Field, SubmissionError, focus} from "redux-form";
 import Input from './input';
 import {required, nonEmpty} from '../../validators';
 
-import "./signup.css";
-
-export class Signup extends React.Component {
+import "./edit-deal.css";
+        
+export class EditDeal extends React.Component {
     onSubmit(values) {
         return fetch('/api/messages', {
-            method: 'POST',
+//NEED TO UPDATE ORIGINAL METHOD: POST
+            method: 'PUT',
             body: JSON.stringify(values),
             headers: {
                 'Content-Type': 'application/json'
@@ -70,43 +71,58 @@ export class Signup extends React.Component {
         }
 
         return (
-            <form className="signup-form"
+            <form className="edit-form"
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)
                 )}>
                 <Field          
-                    name="new-user-name"  
+                    name="dealName"  
                     type="text" 
                     component={Input}
-                    label="User Name"
+                    label="Item Name"
                     validate={[required, nonEmpty]} 
                 />
                 <Field          
-                    name="new-user-first-name"  
+                    name="price"  
                     type="text" 
                     component={Input}
-                    label="First Name"
+                    label="Price"
                     validate={[required, nonEmpty]} 
                 />
                 <Field          
-                    name="new-user-last-name"  
+                    name="image"  
                     type="text" 
                     component={Input}
-                    label="Last Name"
+                    label="Image URL"
                     validate={[required, nonEmpty]} 
                 />
                 <Field          
-                    name="new-password"  
+                    name="seller"  
                     type="text" 
                     component={Input}
-                    label="Password"
+                    label="Seller"
+                    validate={[required, nonEmpty]} 
+                />
+                <Field          
+                    name="productDescription"  
+                    element="textarea"
+                    rows="15" 
+                    component={Input}
+                    label="Product Description"
+                    validate={[required, nonEmpty]} 
+                />
+                <Field          
+                    name="dealLink"  
+                    type="text" 
+                    component={Input}
+                    label="Deal Link"
                     validate={[required, nonEmpty]} 
                 />
                 <button 
                     type="submit"
                     //Here we disable the button if it is pristine (i.e. if the user hasn't entered anything into the field) or it is submitting.
                     disabled={this.props.pristine || this.props.submitting}>
-                    Signup
+                    Update
                 </button>
             </form>
         );
@@ -114,8 +130,8 @@ export class Signup extends React.Component {
 }
 
 export default reduxForm({
-    form: 'signup-form',
+    form: 'edit-form',
     //Automatically focus on first incomplete field when the user submits incorrect value for a field
     onSubmitFail: (errors, dispatch) =>
         dispatch(focus('contact', Object.keys(errors)[0]))
-})(Signup);
+})(EditDeal);
