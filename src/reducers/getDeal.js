1b0d1eda9447330bdf81ee9bed8e4b6edfc2f842
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-06-26 18:51:59 -0700

    Added componentDidMount to pull data from db

---
+++ src/reducers/getDeal.js
@@ -0,0 +1,23 @@
+import * as actions from '../actions/getDeal';
+
+const initialState = {
+    allDeals: []
+};
+
+export const getDealReducer = (state=initialState, action) => {
+    if (action.type === actions.GET_DEALS) {
+        console.log(action);
+        return Object.assign({}, state, {
+        // Object.assign generates
+        // a new state object by merging an object
+        // representing the new state of the lists
+        // to the existing state, and in turn, that resulting 
+        // object into an empty object, which ensures
+        // that we're not mutating the original state object
+        // ...state.dealsItems is the original state
+        // action.deal is the new state     
+        allDeals: [...state.allDeals, ...action.deals.dealItem]
+        })
+    }
+    return state;
+}
