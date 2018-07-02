3b545a6fa5a9bb2db90043a801b97a3697167d7c
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-07-02 08:45:40 -0700

    Hide Edit button if user not logged in

--- src/components/details/product-description.js
+++ src/components/details/product-description.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import EditDeal from '../pages/edit-deal'
+import {connect} from 'react-redux';
+import {clearAuth} from '../../actions/auth';
+import {clearAuthToken} from '../../local-storage';
 
-export default class ProductDescription extends React.Component {
+export class ProductDescription extends React.Component {
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
+    }
     render() {
+        let editButton;
         const editItem = {
             pathname: "/edit-deal/" + this.props.allSalesItems.id,
             id: this.props.allSalesItems.id
         };
+        if (this.props.loggedIn) {
+            editButton = (
+                <Link to={editItem}><button className="btn">Edit</button></Link>
+            )
+        }
 
         const productDetails = (
             <div className="box2">
                 <p>${this.props.allSalesItems.price}</p>
                 <a href={this.props.allSalesItems.dealLink}>{this.props.allSalesItems.dealName}</a>
                 <p>{this.props.allSalesItems.productDescription}</p>
-                <Link to={editItem}><button className="btn">Edit</button></Link>
+                {editButton}
             </div>
         );   
         return (
             <div className="col-8">
-                {productDetails}
+                {productDetails}   
             </div> 
         )
     }
 }
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(ProductDescription);
