b408fc0386dfabb766f9eb75343807fcd6f9bce3
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-07-17 16:23:21 -0700

    Fix menu toggle

--- src/components/landing-page/top-nav.js
+++ src/components/landing-page/top-nav.js
@@ -1,86 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import StoresSubmenu from './stores';
-import CategoriesSubmenu from './categories';
+import Menu from './menu';
 import {connect} from 'react-redux';
-import {clearAuth} from '../../actions/auth';
-import {clearAuthToken} from '../../local-storage';
 import {withRouter} from 'react-router-dom';
 
 import './top-nav.css';
 
 export class TopNav extends React.Component {
-    logOut() {
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
-
     constructor(props) {
         super(props);
         this.state = {
-            showStoresMenu: false,
-            showCategoriesMenu: false
+            showMainMenu: false
         };
     }
-
+    
     handleHover = () => {
-        this.setState({ showStoresMenu: true });
+        this.setState({ showMainMenu: true });
     };
 
     handleLeave = () => {
-        this.setState({ showStoresMenu: false });
+        this.setState({ showMainMenu: false });
     };
 
     toggleDropDown = () => {
-        this.setState({ showStoresMenu: !this.state.showStoresMenu});
-    }
-
-    handleHover2 = () => {
-        this.setState({ showCategoriesMenu: true });
-    };
-
-    handleLeave2 = () => {
-        this.setState({ showCategoriesMenu: false });
-    };
-
-
-
-    toggleDropDown2 = () => {
-        this.setState({ showCategoriesMenu: !this.state.showCategoriesMenu});
+        this.setState({showMainMenu: !this.state.showMainMenu});
     }
 
     render() {
-        let addDealLink;
-        let favoriteLink;
-        let logOutLink;
-        if (this.props.loggedIn) {
-            addDealLink = (
-                <Link to={"/add-deal"} className="addDeal">Add Deal</Link>
-            )
-            favoriteLink = (
-                <Link to={"/favorites"} className="addDeal">Favorites</Link>
-            )
-            logOutLink = (
-                <div className='log-out' onClick={() => this.logOut()}>Log out</div>
-            )
-        } 
         return (
             <nav> 
-                <div className="menu">Menu</div>
-                <ul className="navigation">
-                    <li><Link to={"/"} className="home">Home</Link></li>
-                    <li className="nav__menu-stores" onMouseLeave={this.handleLeave}><a onClick={this.toggleDropDown} onMouseEnter={this.handleHover}>Stores</a>
-                        {this.state.showStoresMenu && <StoresSubmenu />}
-                    </li>
-                    <li className="nav__menu-categories" onMouseLeave={this.handleLeave2}><a onClick={this.toggleDropDown2} onMouseEnter={this.handleHover2}>Categories</a>
-                        {this.state.showCategoriesMenu && <CategoriesSubmenu />}
-                    </li>
-                    <li>{favoriteLink}</li>
-                    <li>{addDealLink}</li>
-                    <li><Link to={"/registration"} className="registration">Sign up</Link></li>  
-                    <li><Link to={"/login"} className="login">Login</Link></li>
-                    <li>{logOutLink}</li>  
-                </ul>
+                <div onMouseLeave={this.handleLeave}><a className="menu" onClick={this.toggleDropDown} onMouseEnter={this.handleHover}>Menu</a>
+                    {this.state.showMainMenu && <Menu />}
+                </div>
             </nav>
         );
     }
