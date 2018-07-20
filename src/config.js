f81fd8f59f84488386ab2a8c39d82cff6dbcde55
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-07-19 21:38:12 -0700

    Update travis.yml

--- src/config.js
+++ src/config.js
@@ -4,4 +4,3 @@ module.exports = {
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
       "http://localhost:3000/api"
   };
-  console.log(process.env.REACT_APP_API_BASE_URL, '&&&&&&&&&&&&&&');
